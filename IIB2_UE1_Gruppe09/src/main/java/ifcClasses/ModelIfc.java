9637a0ee099da74c98a70d31b34f8ccf9def01f3
Melissa Mendoza <mendoza.melissaf@gmail.com>
2018-06-12 23:55:15 +0200

    tried to download file

---
+++ IIB2_UE1_Gruppe09/src/main/java/ifcClasses/ModelIfc.java
@@ -0,0 +1,118 @@
+package ifcClasses;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+import com.apstex.ifc4toolbox.ifc4.*;
+import com.apstex.ifc4toolbox.ifcmodel.*;
+import com.apstex.step.core.*;
+
+import beansDB.Gebaeude;
+import beansDB.Wand;
+
+public class ModelIfc {
+	IfcModel ifcModel;
+	List<Wand> waendeL;
+
+	public ModelIfc() {
+		this.ifcModel = new IfcModel();
+	}
+
+	public ModelIfc(String filePath) {
+		File ifcFile = new File("ifc.ifc");
+		this.ifcModel = new IfcModel();
+
+		try {
+			ifcModel.readStepFile(ifcFile);
+		} catch (Exception e) {
+
+		}
+
+		Collection<IfcWall> waende = ifcModel.getCollection(IfcWall.class);
+		this.waendeL = createWandFromIfc(waende);
+
+	}
+
+	public void createGebaeude(List<Gebaeude> gebL) {
+		IfcBuilding gebIfc;
+		for (Gebaeude g : gebL) {
+			gebIfc = new IfcBuilding();
+			IfcGloballyUniqueId guid = new IfcGloballyUniqueId(g.getGuid());
+			gebIfc.setGlobalId(guid);
+			LIST<IfcLabel> addL =  new LIST<IfcLabel>();
+			addL.add(0, new IfcLabel(g.getStrasse()));
+			addL.add(1, new IfcLabel(g.getHausnummer()));
+			IfcPostalAddress pA = new IfcPostalAddress(null, null, null, null, addL, null, new IfcLabel(g.getOrt()), null,
+					new IfcLabel(String.valueOf(g.getPlz())), null);
+			gebIfc.setBuildingAddress(pA);
+			String pgDMAName = "dma_id";
+			String pgDMADesc = "ID des ersteller DMA";
+			String pgDMAValue = String.valueOf(g.getDma_id());
+			
+			IfcIdentifier namepgDMA = new IfcIdentifier(pgDMAName, false);
+			IfcText descr = new IfcText(pgDMADesc, false);
+			IfcValue valuepgDMA = new IfcText(pgDMAValue);
+			IfcPropertySingleValue pgDMA = new IfcPropertySingleValue(namepgDMA, descr, valuepgDMA, null);
+			
+			IfcGloballyUniqueId propSetGuid = new IfcGloballyUniqueId(ifcModel.getNewGlobalUniqueId());
+			IfcOwnerHistory ownerHistory = new IfcOwnerHistory();
+			IfcLabel name = new IfcLabel( "geb_dma", false);
+			IfcText description = new IfcText("Enthält Information der Ersteller des Gebäudes", false);
+			IfcPropertySetDefinition pgDMASet = new IfcPropertySet(propSetGuid, ownerHistory, name, description, new SET<IfcProperty>());
+			((IfcPropertySet) pgDMASet).addHasProperties(pgDMA);
+			System.out.print(ifcModel.toString());
+			ifcModel.addObject(pgDMA);
+			ifcModel.addObject(pgDMASet);
+			IfcRelDefinesByProperties ifcRelDefProp = new IfcRelDefinesByProperties();
+			ifcRelDefProp.setRelatingPropertyDefinition(pgDMASet);
+			SET<IfcObjectDefinition> tempSet = new SET<IfcObjectDefinition>();
+			tempSet.add(gebIfc);
+			ifcRelDefProp.addAllRelatedObjects(tempSet);
+			ifcModel.addObject(ifcRelDefProp);
+		}
+	}
+
+	private List<Gebaeude> createGeabeudeFromIfc(Collection<IfcBuilding> gebIfcList) {
+		List<Gebaeude> gL = new ArrayList<Gebaeude>();
+		Gebaeude geb;
+		IfcPostalAddress ipA;
+		for (IfcBuilding wIfc : gebIfcList) {
+			geb = new Gebaeude();
+			ipA = wIfc.getBuildingAddress();
+			geb.setPlz(Integer.parseInt(ipA.getPostalCode().getValue()));
+			geb.setOrt(ipA.getTown().getValue());
+			LIST<IfcLabel> address = ipA.getAddressLines();
+			for (IfcLabel l : address) {
+			}
+		}
+		return gL;
+
+	}
+
+	private List<Wand> createWandFromIfc(Collection<IfcWall> waende) {
+		List<Wand> wL;
+		Wand wand;
+		for (IfcWall wIfc : waende) {
+			wand = new Wand();
+
+		}
+		return null;
+	}
+
+	public void getFile(String path) {
+		URL url = getClass().getResource("export/out/test.ifc");
+		String path1 = "export/out/test.ifc"; 
+		try {
+			this.ifcModel.writeStepFile(new File(url.getPath()));
+			System.out.println("Hola");
+		} catch (Exception e){
+			System.out.println("Error with export "+ e.getMessage());
+		}  
+	}
+
+}
---
+++ IIB2_UE1_Gruppe09/src/main/java/webController/IFCController.java
@@ -0,0 +1,68 @@
+package webController;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.stereotype.Controller;  
+
+import beansDB.Dezernatmitarbeiter;
+import beansDB.Gebaeude;
+import ifcClasses.ModelIfc;
+import manage.DBManager;
+
+@Controller
+public class IFCController {
+	@RequestMapping(value = { "/", "ifc" })
+	public String home(HttpServletRequest req, HttpServletResponse res, Model model)
+			throws ClassNotFoundException, SQLException {
+		Dezernatmitarbeiter dma = (Dezernatmitarbeiter) req.getSession().getAttribute("user");
+
+		List<Gebaeude> gebAll = new ArrayList<Gebaeude>(); 
+		DBManager dbm = new DBManager();
+
+		String sql = "SELECT * from gebaeude;";
+		//
+		gebAll = dbm.getGeb(sql);
+ 
+
+		req.getSession().setAttribute("gebaeude", gebAll); // set session attribute
+		model.addAttribute("gebaeude", gebAll); 
+		req.getSession().setAttribute("user", dma); // set session attribute
+		model.addAttribute("user", dma);
+		System.out.println("File" + req.getSession().getAttribute("ifcCreated"));
+
+		return "dezernatmitarbeiter";
+	}
+	@RequestMapping(value = "/readIfcModel", method=RequestMethod.POST)
+	public String addModelfromFile(HttpServletRequest req, HttpServletResponse res, Model model) {
+		 
+		return "redirect:/index.jsp";
+	}
+	
+	@RequestMapping(value = "/exportIfcModel", method=RequestMethod.POST)
+	public String getModelfromDB(HttpServletRequest req, HttpServletResponse res, Model model) throws ClassNotFoundException, SQLException {
+		ModelIfc ifcModel = new ModelIfc();
+		List<Gebaeude> gebL = new ArrayList<Gebaeude>(); 
+		DBManager dbm = new DBManager();
+
+		String sql = "SELECT * from gebaeude;";
+		String path = "/out/ifcM.ifc";
+		gebL = dbm.getGeb(sql);
+		ifcModel.createGebaeude(gebL);
+		ifcModel.getFile(path);
+		Boolean ifcCreated = true;
+		model.addAttribute("ifcCreated", ifcCreated);
+		return "dezernatmitarbeiter";
+	}
+	
+
+}
